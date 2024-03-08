6a69446d19385b1ba308fc0e5eb590e90a199bea
ESWARA RAO VETLA <112643496+ESWARVETLA-19@users.noreply.github.com>
2024-03-08 13:37:15 +0530

    Create Solution_java.java

---
+++ Minimum_Boxes/Solution_java.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int apples;
+      apples=sc.nextInt();
+      int boxes=(apples+1)/2;
+      System.out.print(boxes);
+  }
+}
